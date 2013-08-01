96717669900367f1baf681f9166b68bd3910fcb5
Elderry <Elderry@outlook.com>
2013-08-01 17:40:00 +0800

    Added query specification

---
+++ src/Node.py
@@ -0,0 +1,13 @@
+class Node(object):
+    '''
+    Controller of all nodes.
+    '''
+    
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        self.ID=0
+    
+    def execute(self, query):
+        pass
