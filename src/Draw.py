6983a896b9ea1a7668c2fc3bfdc4eaa0f52a2491
zhang yang <zy3861@163.com>
2013-08-26 16:12:57 +0800

    use .bat to run the script

--- src/Draw.py
+++ src/Draw.py
@@ -4,27 +4,26 @@ Created on 2013-8-24
 @author: hp
 '''
 
-import subprocess
-import time
 import numpy as np
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
-    """
-    p = subprocess.Popen('python chord.py -i ini', stdin = subprocess.PIPE, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
-    print p.stdout.read()
-    time.sleep(1)
-    p.stdin.write('show ID')
-    print p.stdout.read()
-    p.kill()
-    """
     maxv = []
     minv = []
     avg = []
     num = 0
     data = open("sample.txt")
     for line in data:
-        tmp = map(int,line.split(','))
+        if not line.strip():
+            if num != 0:
+                maxv.append(0)
+                minv.append(0)
+                avg.append(0)
+                num += 1
+            continue
+        sta = line.split(',')
+        sta.pop()
+        tmp = map(int,sta)
         maxv.append(max(tmp))
         minv.append(min(tmp))
         av = sum(tmp) / len(tmp)
@@ -32,8 +31,9 @@ if __name__ == '__main__':
         num += 1
     err = np.row_stack((np.array(avg) -  np.array(minv),  np.array(maxv) -  np.array(avg)))
     plt.errorbar(range(1,num +1), avg, fmt='ro', yerr=err)
-    plt.xticks(range(0, num +2))
-    plt.yticks(range(min(minv) -1, max(maxv) +2))
+    plt.xlim(0, num +1)
+    plt.ylim(0, max(maxv) +1)
     plt.xlabel(r'nodes')
     plt.ylabel(r'queries')
     plt.show()
+    data.close()
