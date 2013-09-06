d26ed9c0b2a05d490fc18aafcb9e1f140875d98d
zhang yang <zy3861@163.com>
2013-09-06 20:58:45 +0800

    GUI

--- src/Draw.py
+++ src/Draw.py
@@ -1,43 +1,75 @@
 '''
-Created on 2013-8-24
+Created on 2013-9-6
 
 @author: hp
 '''
 
-from __future__ import division
-import numpy as np
-import matplotlib.pyplot as plt
+import pygame
+import sys
+import math
+import thread
+from twisted.internet import protocol, reactor
+from pickle import loads
+
+class Draw(protocol.Protocol):
+    def dataReceived(self, rawData):
+        data=loads(rawData)
+        query = data[0]
+        nid = data[1]
+        nextid = data[2]
+        e = pygame.event.Event(pygame.USEREVENT,nid=int(nid), query=int(query), nextid=int(nextid))
+        pygame.event.post(e)
+
+class DrawFactory(protocol.ServerFactory):
+    def buildProtocol(self, addr):
+        return Draw()
+
+def paint():
+    nodeNum = 2 ** 6
+    nodelist = []
+    r = 300
+    pygame.init()
+    screen = pygame.display.set_mode([640, 640])
+    pygame.display.set_caption('chord demo')
+    screen.fill((255,255,255))
+    pygame.draw.circle(screen, (128,128,128), [320, 320], r, 1)
+    myfont = pygame.font.SysFont("Comic Sans MS", 10)
+    clock = pygame.time.Clock()
+    for i in range(nodeNum):
+        pygame.draw.circle(screen,(0,0,0),(int(320 + r * math.cos(i * 2 * math.pi/nodeNum)),int(320 + r * math.sin(i * 2 * math.pi/nodeNum))),3,0)
+        screen.blit(myfont.render(str(i),1,(255,0,0)),(int(320 + (r-20) * math.cos(i * 2 * math.pi/nodeNum)),int(320 + (r-20) * math.sin(i * 2 * math.pi/nodeNum))))
+    while True:
+        clock.tick(30)
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:  
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    screen.fill((255,255,255))
+                    pygame.draw.circle(screen, (128,128,128), [320, 320], r, 1)
+                    for i in range(nodeNum):
+                        if i in nodelist:
+                            pygame.draw.circle(screen,(0,255,0),(int(320 + r * math.cos(i * 2 * math.pi/nodeNum)),int(320 + r * math.sin(i * 2 * math.pi/nodeNum))),5,0)
+                        else:
+                            pygame.draw.circle(screen,(0,0,0),(int(320 + r * math.cos(i * 2 * math.pi/nodeNum)),int(320 + r * math.sin(i * 2 * math.pi/nodeNum))),3,0)
+                        screen.blit(myfont.render(str(i),1,(255,0,0)),(int(320 + (r-20) * math.cos(i * 2 * math.pi/nodeNum)),int(320 + (r-20) * math.sin(i * 2 * math.pi/nodeNum))))
+            elif event.type==pygame.USEREVENT:
+                nid = event.nid
+                query = event.query
+                nextid = event.nextid
+                if query == 12:
+                    nodelist.append(nid)
+                    pygame.draw.circle(screen,(0,255,0),(int(320 + r * math.cos(nid * 2 * math.pi/nodeNum)),int(320 + r * math.sin(nid * 2 * math.pi/nodeNum))),5,0)
+                elif type == 13:
+                    if(nid not in nodelist):
+                        pygame.draw.circle(screen,(0,0,255),(int(320 + r * math.cos(nid * 2 * math.pi/nodeNum)),int(320 + r * math.sin(nid * 2 * math.pi/nodeNum))),3,0)
+                    pygame.draw.line(screen,(0,0,255),(int(320 + r * math.cos(id * 2 * math.pi/nodeNum)),int(320 + r * math.sin(id * 2 * math.pi/nodeNum))),(int(320 + r * math.cos(nextid * 2 * math.pi/nodeNum)),int(320 + r * math.sin(nextid * 2 * math.pi/nodeNum))))
+                else:
+                    pass
+        pygame.display.update()
 
 if __name__ == '__main__':
-    maxv = []
-    minv = []
-    avg = []
-    num = 0
-    data = open("sample.txt")
-    for line in data:
-        if not line.strip():
-            if num != 0:
-                maxv.append(0)
-                minv.append(0)
-                avg.append(0)
-                num += 1
-            continue
-        sta = line.split(',')
-        sta.pop()
-        tmp = map(int,sta)
-        maxv.append(max(tmp))
-        minv.append(min(tmp))
-        av = sum(tmp) / len(tmp)
-        avg.append(av)
-        num += 1
-    err = np.row_stack((np.array(avg) -  np.array(minv),  np.array(maxv) -  np.array(avg)))
-    plt.figure(1)
-    plt.errorbar(range(1,num +1), avg, fmt='ro', yerr=err)
-    plt.xlim(0, num +1)
-    plt.ylim(0, max(maxv) +1)
-    plt.xlabel(r'nodes')
-    plt.ylabel(r'queries')
-    plt.plot(2)
-    plt.plot(range(1,num +1),np.log2(range(1,num +1)))
-    plt.show()
-    data.close()
+    thread.start_new_thread(paint,())
+    reactor.listenTCP(9000, DrawFactory())
+    reactor.run()
+    
